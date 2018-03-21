eb8aa8dc4ef52b461a831ff4f01f06fbb5339458
Perajit Pasupat <perajit.p@gmail.com>
2018-03-22 04:21:21 +0700

    Initialize project
    - Setup webpack / babel / eslint

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  target: 'node',
+  entry: {
+    content: './src/content/index.js',
+    popup: './src/popup/index.js'
+  },
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Property Reporter Extension',
+      filename: 'popup.html',
+      template: 'src/popup-template.html',
+      chunks: ['popup'],
+    }),
+    new CopyWebpackPlugin([
+      'src/manifest.json',
+      'resources/*'
+    ])
+  ]
+}
