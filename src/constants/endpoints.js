28a08cbb0f8c05b8d7651b69c4b76afad2233350
Perajit Pasupat <perajit.p@gmail.com>
2018-04-24 10:09:29 +0700

    Change api endpoints to firebase instead of localhost

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,5 +1,6 @@
-const API_ENDPOINT = 'http://localhost:5000/api' // FIXME
+const HOSTING_URL = 'https://firestore-prop.firebaseapp.com'
+const API_ROOT = `${HOSTING_URL}/api`
 
-export const API_PROPERTIES_ENDPOINT = `${API_ENDPOINT}/properties`
-export const API_PROPERTY_SUMMARY_ENDPOINT = `${API_ENDPOINT}/properties/summary`
+export const API_PROPERTIES_ENDPOINT = `${API_ROOT}/properties`
+export const API_PROPERTY_SUMMARY_ENDPOINT = `${API_ROOT}/properties/summary`
 export const DATA_SOURCE_ENDPOINT = 'www.ddproperty.com/รวมประกาศขาย'
